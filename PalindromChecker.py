a0a6e2319be4ae8486dd006934b4e1ff192e14c3
ProvincialKiD <provincialkid@gmail.com>
2018-04-22 20:49:14 +0600

    new update in python file "palindrome Checker"

--- PalindromChecker.py
+++ PalindromChecker.py
@@ -1,11 +1,23 @@
-string=input("Enter the text here:")
-length=len(string)-1
-for index in range(len(string)):
-    if string[index] != string[length]:
-        print("Not palindrom!!")
-        break
-    else:
-        length=length-1
-        if length>index:
-            print("paliindrom")
-            break
+string = input("Enter the text here:")
+# My First Version
+#length = len(string) - 1
+# for index in range(len(string)):
+#     if string[index] != string[length]:
+#         print("Not palindrom!!")
+#         break
+#     else:
+#         length = length - 1
+#         if length > index:
+#             print("paliindrom")
+#             break
+
+# Second version Adapted from web
+def isPalindrome(str):
+    # Run loop from 0 to len/2
+    for i in range(int(len(str) / 2)):
+        if str[i] != str[len(str) - i - 1]:
+            return False
+    return True
+
+
+print(isPalindrome(string))
